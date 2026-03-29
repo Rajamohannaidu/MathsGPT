ff61ee9306024f959e3f7c9a2c15a918297e6e1a
Rajamohannaidu <rajamohan4urs@gmail.com>
2026-03-29 12:41:56 +0530

    update

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ st.set_page_config(
     page_icon="🧮"
 )
 st.title("🧮 Text To Math Problem Solver Using Llama 3")
-st.secrets["GROQ_API_KEY"]
 
 groq_api_key = st.sidebar.text_input(label="Groq API Key", type="password")
 
